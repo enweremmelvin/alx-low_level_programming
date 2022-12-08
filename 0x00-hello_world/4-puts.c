e4eecd0bc55ecc39d3e928c919f2bd5123b8309c
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-08 08:42:04 -0800

    Added answer fole for - 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This program prints and returns 0
+ * try it
+ */
+int main(void)
+{
+	char output[] = "\"Programming is like building a multilingual puzzle";
+	puts(output);
+	return (0);
+}
