09601ce0c5c66abfb3e4ec56ab6c1a959813df2a
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-08 20:01:04 +0100

    Added answer file for - 8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Successful)
+ */
+int main(void)
+{
+	int year, month, day;
+
+	year = 2015;
+	month = 10;
+	day = 19;
+
+	printf("and that piece of art is useful\" - Dora Korpar,");
+	printf(" %d-%d-%d\n", year, month, day);
+
+	return (1);
+}
