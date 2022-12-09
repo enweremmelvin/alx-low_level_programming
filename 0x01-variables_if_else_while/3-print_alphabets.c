10aa28a4a33a65b88b6725853615a9d7b59b9ee7
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-09 11:12:31 -0800

    added anwer file for - 3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int lower;
+	int upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+		putchar(lower);
+	
+	for (upper = 'A'; opper <= 'Z'; upper++)
+		putchar(upper);
+
+	putchar('\n');
+	return (0);
+}
