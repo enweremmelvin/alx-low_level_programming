5a2ced1662d6e654ac150bb61b540999b6347b03
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-13 12:12:41 +0100

    Added answer file for - 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdout.h>
+#include "_putchar.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: In this program; 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar(' ');
+	
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the directory for the third alx project on c
