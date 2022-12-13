5e9202c1c5499bc97ce16120f4d8270c49e8e386
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-13 22:05:05 +0100

    Added answer file for task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - make a time table out of the given number
+ *
+ * @n: value to be considered
+ */
+void print_times_table(int n)
+{
+	int rows;
+	int cols;
+	if (!(n > 15 || n < 0))
+	{
+		for (rows = 0; rows <= n; rows++)
+		{
+			for (cols = 0; cols <= n; cols++)
+			{
+				if (cols == 0)
+				{
+					printf("%d", rows * cols);
+				}
+				else
+				{
+					printf("%3d", rows *cols);
+				}
+
+				if (cols == n)
+					continue;
+			
+				printf(",   ");
+			}
+
+			printf("\n");
+		}
+	}
+}
