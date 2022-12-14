726054a4ea3b96db736d69fedc3c8bca7eac7e0e
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-14 12:53:44 +0100

    made changes

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,20 +12,14 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b && b > c) || (a > c && c > b))
-	{
+	if (a > b && a > c)
 		largest = a;
-	}
 
-	else if ((b > a && a > c) || (b > c && c > a))
-	{
+	if (b > a && b > c)
 		largest = b;
-	}
 
-	else if ((c > a && a > b) || (c > b && b > a))
-	{
+	if (c > a && c > b)
 		largest = c;
-	}
 
 	return (largest);
 }
