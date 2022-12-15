a87c54a75dd321850c1c7f0f6516ed3f2d37007a
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-15 21:42:13 +0100

    Added answer file for task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangles
+ * @size: size of our triangle
+ */
+
+void print_triangle(int size)
+{
+	int counter;
+	int inner_counter;
+	int whitespace;
+
+	counter = 1;
+
+	while (counter <= size)
+	{
+		inner_counter = 1;
+		whitespace = counter;
+
+		while (whitespace < size)
+		{
+			_putchar(' ');
+			whitespace++;
+		}
+
+		while (inner_counter <= counter)
+		{
+			_putchar('#');
+			inner_counter++;
+		}
+
+		_putchar('\n');
+		counter++;
+	}
+}
