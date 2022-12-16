94215b45deea8867cf7dd316b055af341c3a0cdc
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-16 12:30:20 +0100

    made changes to task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,33 +7,31 @@
 
 void print_number(int n)
 {
-	if (n >= 0 && n < 10)
-	{
-		_putchar(n + '0');
-	}
-	else if (n < 0)
+	unsigned int a;
+	unsigned int b;
+	unsigned int counter;
+
+	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
-
-		if (n > 9)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		else
-		{
-			_putchar(n + '0');
-		}
-	}
-	else if (n >= 10 && n < 100)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		a = n * -1;
 	}
 	else
 	{
-		print_number(n / 10);
-		_putchar((n % 10) + '0');
+		a = n;
+	}
+
+	b = a;
+	counter = 1;
+
+	while (b > 9)
+	{
+		b = b / 10;
+		counter = counter * 10;
+	}
+
+	for (; counter >= 1; counter /= 10)
+	{
+		_putchar(((a / counter) % 10) + 48);
 	}
 }
