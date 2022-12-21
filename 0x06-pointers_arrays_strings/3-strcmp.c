7f23059b2a438b8e19509eadfaf60ce88d15b5d2
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-21 10:29:19 +0100

    Added script for task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strcmp - compare provided strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: return result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result = strcmp(s1, s2);
+
+	return (result);
+}
