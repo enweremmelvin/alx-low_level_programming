9d095246d70113e50a3ddca89bda36cfcaa78df4
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-21 19:14:47 +0100

    Added answer file for task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * *cap_string - capitalise each word of string
+ * @string: string to be worked on
+ *
+ * Return: return parsed string
+ */
+
+char *cap_string(char *string)
+{
+	char test[] = {
+		' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'
+	};
+
+	char *ptr = test;
+
+	unsigned long int i;
+	int curr;
+	int length;
+	int counter;
+	int next;
+
+	length = strlen(string);
+
+	for (counter = 0; counter < length; counter++)
+	{
+		curr = string[counter];
+		next = string[counter + 1];
+
+		for (i = 0; i < strlen(ptr); i++)
+		{
+			if ((curr == (ptr[i])) && next >= 97 && next <= 122)
+				string[counter + 1] -= 32;
+		}
+	}
+
+	return (string);
+}
