e43c661f7c7abc780f90b8ba4b4df89b7991879c
enweremmelvin <enweremmelvinconfidence@gmail.com>
2022-12-21 09:46:50 +0100

    Added script for task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * *_strcat - concatenate characters
+ * @*dest: destination character
+ * @*src: source character
+ *
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *result = strcat(dest, src);
+
+	return (result);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is my directory for the seventh project on c
