bb6674460f802d22981f92ba78f64f1ed316aaa7
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-02 23:04:08 +0100

    made changes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,28 +12,5 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j, match;
-	char *ptr;
-
-	for (i = 0; i < strlen(needle); i++)
-	{
-		for (j = 0; j < strlen(haystack); j++)
-		{
-			if (needle[i] == haystack[j])
-			{
-				match = 1;
-				ptr = &haystack[j];
-
-				break;
-			}
-		}
-
-		if (match == 1)
-			break;
-	}
-
-	if (match == 1)
-		return (ptr);
-	else
-		return (NULL);
+	return(strstr(haystack, needle));
 }
