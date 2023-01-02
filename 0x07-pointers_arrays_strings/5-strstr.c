47f833463b5a55816acd9252750437170e1c9cd6
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-02 20:42:27 +0100

    Added answer script for task 6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include <string.h>
+
+/**
+ * *_strstr - find the needle in the haystack
+ *
+ * @haystack: where to search
+ * @needle: what to search for
+ *
+ * Return: return pointer to first occurence
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	int match;
+	char *ptr;
+
+	i = 0;
+
+	while (needle[i] != '\0')
+	{
+		while (haystack[j] != '\0')
+		{
+			if (needle[i] == haystack[j])
+			{
+				match = 1;
+				ptr = &haystack[j];
+
+				break;
+			}
+
+			j++;
+		}
+
+		if (match == 1)
+			break;
+
+		i++;
+	}
+
+	if (match == 1)
+		return (ptr);
+	else
+		return (NULL);
+}
