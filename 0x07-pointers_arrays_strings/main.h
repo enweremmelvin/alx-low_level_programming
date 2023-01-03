e33f11b39cf2820a3c79559617a18663e54d93a9
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-03 12:14:29 +0100

    Added answer script for task 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char board[8][8]);
+void print_diagsums(int *a, int size);
