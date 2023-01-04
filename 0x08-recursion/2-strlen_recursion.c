5bc130ede05ad036295609dda095a6408df98011
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-04 15:38:29 +0100

    made changes

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strlen_recursion - find the lengt of a string
+ *
+ * @s: string to be searched
+ *
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = strlen(s);
+
+	return(i);
+}
