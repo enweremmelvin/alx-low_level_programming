a5de614c16f1193f8bb2aced648239faf339bf75
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-07 11:21:17 +0100

    made changes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,14 +14,31 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int result;
+	int chknum;
 
 	result = 0;
+	chknum = 0;
 
+	/* check if only one argument (function name) is passed*/
 	if (argc == 1)
 	{
 		printf("0\n");
 	}
 
+	/* check if args are passed but don't contain a digit */
+	for (i = 0; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+			chknum++;
+	}
+
+	if (chknum == argc)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	/* iterate over argc and print result or return error if char exists*/
 	for (i = 1; i < argc; i++)
 	{
 		if (atoi(argv[i]) == 0)
