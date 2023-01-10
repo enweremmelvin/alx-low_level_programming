60781fcdc1ee4e08c61be33f612764a9cdcbac53
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-10 07:07:13 +0100

    made changes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < width; i++)
 	{
-		array[i + 1] = malloc(height * sizeof(int));
+		array[i] = malloc(height * sizeof(int));
 
 		if (array[i] == NULL)
 		{
