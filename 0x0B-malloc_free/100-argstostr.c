791402046c163a42f809d354b87a46f953297b1b
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-10 09:50:26 +0100

    added script for task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * argstostr - concatenate arguments
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: address to new string or null on failure
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0;
+	int length;
+	int total_length;
+
+	char *concat;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		total_length += strlen(av[i]);
+
+	concat = malloc(total_length * sizeof(char) + ac);
+
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		length = strlen(av[i]);
+
+		for (j = 0; j < length; j++)
+		{
+			concat[k] = av[i][j];
+
+			k++;
+		}
+
+		concat[k] = '\n';
+		k++;
+	}
+
+	return (concat);
+}
