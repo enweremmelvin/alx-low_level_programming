b0eeae35ba8928fef8425c1af806d8a8ce82fc4b
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-13 03:33:05 +0100

    fixed segmentation fault issues

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,15 +11,20 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	char *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return ('\0');
 
-	ptr = calloc(nmemb, size);
+	/* ptr = calloc(nmemb, size); */
+	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
 		return ('\0');
+
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
 
 	return (ptr);
 }
