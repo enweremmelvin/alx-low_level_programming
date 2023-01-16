d9a4b47e739c8eee9dc40ca9949e2d1d90448d63
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-16 18:10:30 +0100

    solution for task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,23 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
+};
+
+/**
+ * struct new_dog - create a new struct of type new_dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+typedef struct new_dog
+{
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
