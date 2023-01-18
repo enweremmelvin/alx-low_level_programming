5651709db0215ec28a0a8fe73296b0860a0bc9fb
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-18 11:11:51 +0100

    solution for task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <string.h>
+
+/**
+ * int_index - check if element at index of supplied array is an int
+ *
+ * @array: array whose elements are to be considered
+ * @size: size of array
+ * @cmp: pointer to function (callback)
+ *
+ * Return: integer value
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int retval;
+
+	if (size <= 0)
+		return (-1);
+
+	for (; i < size; i++)
+	{
+		if (array[i])
+		{
+			retval = cmp(array[i]);
+
+			if (retval != 0)
+				return (i);
+		}
+	}
+
+	return (-1);
+}
