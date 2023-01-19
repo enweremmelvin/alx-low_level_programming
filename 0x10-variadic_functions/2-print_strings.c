fd5cb3e940232373f7cf9f8522e21343a6be0029
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-19 15:32:31 +0100

    fix for failing check

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (strcmp(string, "NULL") == 0)
 			printf("(nil)");
-		else
+		if (strcmp(string, "NULL") != 0)
 			printf("%s", string);
 
 		if ((separator != NULL) && (i < n - 1))
