69c1b37795cf1d85fddd8b5f764062b8ee077359
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-19 22:36:29 +0100

    solution for task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_all - print specified input
+ *
+ * @format: format identifier
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+	char *string, *spec = "cifs";
+	unsigned int i = 0, j;
+
+	va_start(arg, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (spec[j]) /* loop for seperator */
+		{
+			if ((spec[j] == format[i]) && i != 0)
+			{
+				printf(", ");
+				break;
+			}
+			j++;
+		}
+		switch (format[i]) /* switch statement to match identifiers*/
+		{
+		case 'c':
+			printf("%c", va_arg(arg, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(arg, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(arg, double));
+			break;
+		case 's':
+			string = va_arg(arg, char *);
+			if ((!string) || (strcmp(string, "NULL") == 0))
+				printf("(nil)");
+			else
+				printf("%s", string);
+			break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(arg);
+}
