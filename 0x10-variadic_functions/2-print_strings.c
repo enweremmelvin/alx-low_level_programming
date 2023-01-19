306829b1824ab744eb1b4f104f919da2e73cefca
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-19 15:07:12 +0100

    fix failing check

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", string);
 
-		if ((strcmp(separator, "NULL") != 0) && (i < n - 1))
+		if ((separator != NULL) && (i < n - 1))
 		{
 			for (j = 0; j < strlen(separator); j++)
 				printf("%c", separator[j]);
