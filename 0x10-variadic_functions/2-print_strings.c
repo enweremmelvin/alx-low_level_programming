6b5d0279317f73cfb757a19d892ecab2f01900c4
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-19 15:12:14 +0100

    fix failing check

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,6 +15,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list strarg;
 	unsigned int i, j;
 
+	if (n == 0)
+		return;
+
 	va_start(strarg, n);
 
 	for (i = 0; i < n; i++)
