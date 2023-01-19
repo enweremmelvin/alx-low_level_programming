03c8bc5c757e81fb9c606f33d2ef191157c57a0a
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-19 08:55:43 +0100

    solution for task 1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all arguments passed
+ *
+ * @n: number of arguments
+ * @...: variadic arg
+ *
+ * Return: result of sum or 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int total;
+	va_list arg;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	total = 0;
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+		total += va_arg(arg, int);
+
+	return (total);
+}
