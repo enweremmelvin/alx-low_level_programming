6229fd107118d719b5fc015bb205db602c67da40
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-26 22:21:13 +0100

    solution for first advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * pre_print - constructor function to print data
+ * before the execution of main functino
+ */
+
+void pre_print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
