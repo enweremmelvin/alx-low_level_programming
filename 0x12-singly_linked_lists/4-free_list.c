3d356869397f975f86dbcf2c53c77d43a3c1b969
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-26 21:56:59 +0100

    solution for task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - free the total amount of memory allocated to a list
+ *
+ * @head: pointer to head of list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *curr;
+
+	if (head != NULL)
+	{
+		curr = head;
+		head = head->next;
+		free(curr);
+
+		/* use recursion to free allocated memory */
+		free_list(head);
+	}
+}
