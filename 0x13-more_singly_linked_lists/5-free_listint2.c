25b3e7d356d628249453f22a121f57171de8dde6
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-30 14:51:39 +0100

    solution for task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free memory allocated to nodes of a list
+ * and set head to NULL
+ *
+ * @head: head of linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+
+		free(temp);
+	}
+
+	*head = NULL;
+}
