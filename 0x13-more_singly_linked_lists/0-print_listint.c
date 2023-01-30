8fa12d654223aabeef3bb9385449b9a4deb8a88e
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-30 13:03:51 +0100

    solution for task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all the elements of a listint list
+ *
+ * @h: head of the linked list (holds pointer to first node);
+ *
+ * Return: total number of nodes in the linked list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+
+		count++;
+	}
+
+	return count;
+}
