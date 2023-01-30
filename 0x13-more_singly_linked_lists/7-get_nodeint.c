6a24c90646b1027a82cc66b1f71cc2daf37a09c8
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-30 16:03:05 +0100

    solution for task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * *get_nodeint_at_index - get node at specified index
+ *
+ * @head: head of linked list
+ * @index: index of node in linked list
+ *
+ * Return: return address of node at index or null if the node does'nt exist
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *temp;
+
+	i = 0;
+	temp = head;
+
+	for (; i < index; i++)
+	{
+		if (!temp)
+			return (NULL);
+		else
+			temp = temp->next;
+	}
+
+	return (temp);
+}
