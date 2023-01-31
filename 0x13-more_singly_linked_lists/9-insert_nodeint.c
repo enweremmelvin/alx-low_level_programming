ec4348c9706ad7e4dac64d37737a6ffece351e3e
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-01-31 11:23:04 +0100

    made changes to fix failing checks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new_node_at_index == NULL)
 		return (NULL);
-	if (idx = 0)
+	if (idx == 0)
 		return (NULL);
 
 	while (i < idx)
