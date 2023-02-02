0ad8337f3b78f0b7121ee6b35daf4832fda4a102
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-02-02 16:29:46 +0100

    fix for failing checks

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,11 +13,8 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int result;
-	result = (n >> index) & 1;
-
-	if (result != 1 && result != 0)
+	if (index > 53)
 		return (-1);
-	else
-		return ((n >> index) & 1);
+
+	return ((n >> index) & 1);
 }
