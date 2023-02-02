1bd31a001f59aa9b81162aa76d5c5aaa30245b73
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-02-02 12:24:22 +0100

    solution for task 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * include frequently used standard libraries
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * prototypes of functions
+ */
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
