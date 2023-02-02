4bcdd3a96db5021ad2f20d3bf16e99bb62d3f53f
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-02-02 16:24:17 +0100

    solution for task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - get the bit at a given index
+ *
+ * @n: number to be converted to bits and parsed
+ *     to get a bit at certain index
+ *
+ * @index: index of bit to be returned
+ *
+ * Return: bit found at given index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return ((n >> index) & 1);
+}
