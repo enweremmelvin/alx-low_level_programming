57718fdca11a2189fe80633542de0eadde203312
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-02-06 21:00:46 +0100

    solution for failing checks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		printf("Entered this condition\n");
 		open_ret_val = open(filename, O_CREAT, 0600);
 
 		if (open_ret_val == -1)
