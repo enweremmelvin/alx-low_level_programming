0b642d761befaf290cb4199d5b79eb6a3011b900
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-02-06 16:49:12 +0100

    solution for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - create a specified file with filename (filename)
+ *
+ * @filename: name of file to be created
+ * @text_content: contents of file
+ *
+ * Return: 1 on success; -1 on any failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int length;
+	int open_ret_val;
+	int write_ret_val;
+
+	if (filename == NULL)
+		return (-1);
+
+	length = strlen(text_content);
+	open_ret_val = open(filename, O_CREAT | O_RDWR, 0600);
+
+	/**
+	 * check if text_content is NULL and
+	 * end the program after creating empty file
+	 */
+
+	if (text_content == NULL)
+	{
+		close(open_ret_val);
+		return (1);
+	}
+
+	write_ret_val = write(open_ret_val, text_content, length);
+
+	if (write_ret_val == -1)
+		return (-1);
+
+	close(open_ret_val);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,8 +5,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* declare prototypes of custom functions */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
