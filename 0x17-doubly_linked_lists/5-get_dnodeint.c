1111936086df277f26ad8954edfbaea04519a3a2
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-16 12:45:38 +0100

    solution to task 6

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get the address of the node at index (index)
+ *
+ * @head: head of the doubly linked list
+ * @index: index of node to be gotten
+ *
+ * Return: address of the node at given index; NULL if the node does not exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int node_count = 0;
+
+	while (temp != NULL)
+	{
+		node_count += 1;
+		temp = temp->next;
+	}
+
+	if (index > node_count)
+		return (NULL);
+
+	temp = head;
+	node_count = -1;
+
+	while (temp != NULL)
+	{
+		node_count += 1;
+
+		if (node_count == index)
+			break;
+		temp = temp->next;
+	}
+
+	return (temp);
+}
