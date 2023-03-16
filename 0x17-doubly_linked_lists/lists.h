dfb042df7b0e2ffed9fb52ebe85a5cec4f4722e0
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-16 11:13:09 +0100

    solution to task 2

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -11,3 +11,4 @@ typedef struct dlistint_t
 /* function prototypes */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
