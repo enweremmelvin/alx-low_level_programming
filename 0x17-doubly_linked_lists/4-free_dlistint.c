4973a5052f95fa9a631092ab161cdaabcf54dfe0
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-16 13:21:22 +0100

    solution for task 8

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,11 +2,9 @@
 #include "lists.h"
 
 /**
- * free_dlisting - free memory allocated for a doubly linked list
+ * free_dlistint - free memory allocated for a doubly linked list
  *
  * @head: head of the linked list
- *
- * Return: nothing
  */
 
 void free_dlistint(dlistint_t *head)
@@ -21,6 +19,4 @@ void free_dlistint(dlistint_t *head)
 		free(head);
 		head = temp;
 	}
-
-	return;
 }
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * insert_dnodeint_at_index - insert a new node at a given position
+ *
+ * @h: head of linked list
+ * @idx: index to insert new node at
+ * @n: value for n field of new node
+ *
+ * Return: address of new node; NULL on failure
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp;
+	dlistint_t *new_node;
+	unsigned int node_index;
+
+	if (*h == NULL)
+		return (NULL);
+
+	temp = *h;
+	node_index = 0;
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	while (temp != NULL)
+	{
+		if (idx == node_index)
+		{
+			new_node->n = n;
+			new_node->next = temp;
+			new_node->prev = temp->prev;
+			temp->prev->next = new_node;
+			temp->prev = new_node;
+
+			break;
+		}
+
+		node_index += 1;
+		temp = temp->next;
+	}
+
+	return (new_node);
+}
