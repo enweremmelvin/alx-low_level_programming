ebf5a5eecfa4c1b8830d74b48cc422cf64839bdb
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-03-17 12:31:34 +0100

    correction for task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,45 +14,68 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp;
-	dlistint_t *new_node;
-	unsigned int node_index;
+	dlistint_t *temp, *new_node;
+	unsigned int node_index, list_len;
 
 	temp = *h;
 	node_index = 0;
 	new_node = malloc(sizeof(dlistint_t));
+	new_node->n = n;
+	list_len = dlistint_len(*h);
 
 	if (new_node == NULL)
 		return (NULL);
-
+	if (*h == NULL)
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*h = new_node;
+		return (new_node);
+	}
 	while (temp != NULL)
 	{
-		if (idx == node_index)
+		if (idx == 0)
 		{
-			if (node_index > 0)
-			{
-				new_node->n = n;
-				new_node->next = temp;
-				new_node->prev = temp->prev;
-				temp->prev->next = new_node;
-				temp->prev = new_node;
-			}
-			if (node_index == 0)
-			{
-				new_node->n = n;
-				new_node->next = temp;
-				new_node->prev = NULL;
-				*h = new_node;
-			}
+			new_node->next = *h;
+			new_node->prev = (*h)->prev;
+			*h = new_node;
 			break;
+		} else if ((idx == list_len) && (node_index == idx - 1))
+		{
+			new_node->next = temp->next;
+			new_node->prev = temp;
+			temp->next = new_node;
+		} else if (idx == node_index && idx != list_len)
+		{
+			new_node->next = temp;
+			new_node->prev = temp->prev;
+			temp->prev->next = new_node;
 		}
 		node_index += 1;
 		temp = temp->next;
-	}
-	if (idx > node_index)
+	} return (new_node);
+}
+
+/**
+ * dlistint_len - get the number of elements in a linked list
+ *
+ * @h: head of the linked list
+ *
+ * Return: Number of elements in the linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int node_count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
 	{
-		free(new_node);
-		return (NULL);
+		node_count += 1;
+		h = h->next;
 	}
-	return (new_node);
+
+	return (node_count);
 }
