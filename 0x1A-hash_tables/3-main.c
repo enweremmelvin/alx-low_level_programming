a90e9381b4c259c9c94efcb9259d8e53882e904f
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-04-07 12:28:58 +0100

    solution for task 3 - 3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -10,29 +10,9 @@
  */
 int main(void)
 {
-	int retval;
-	hash_table_t *ht;
+    hash_table_t *ht;
 
-	ht = hash_table_create(1024);
-	hash_table_set(ht, "betty", "cool");
-
-	retval = key_index((unsigned char *)"betty", 1024);
-	printf("BETTY key index: %u\n", retval);
-	printf("Key: %s - Value: %s\n", ht->array[retval]->key, ht->array[retval]->value);
-
-
-	hash_table_set(ht, "hetairas", "cool");
-
-	retval = key_index((unsigned char *)"hetairas", 1024);
-	printf("HETAIRAS key index: %u\n", retval);
-	printf("Key: %s - Value: %s\n", ht->array[retval]->key, ht->array[retval]->value);
-
-
-	hash_table_set(ht, "mentioner", "cool");
-
-	retval = key_index((unsigned char *)"mentioner", 1024);
-	printf("MENTIONER key index: %u\n", retval);
-	printf("Key: %s - Value: %s\n", ht->array[retval]->next->key, ht->array[retval]->next->value);
-
-	return (EXIT_SUCCESS);
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "betty", "cool");
+    return (EXIT_SUCCESS);
 }
