e322f09eff2917fd0c95fd16ee3b763bdca4af87
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-04-07 13:08:14 +0100

    solution for task 3 - 3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,8 +21,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	if (strlen(key) == 0)
-		return (0);
 
 	index = key_index((unsigned char *) key, ht->size);
 
