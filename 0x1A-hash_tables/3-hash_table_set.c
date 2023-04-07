970f706f2961f22c2424a6cd1c91ec232d197f90
enweremmelvin <enweremmelvinconfidence@gmail.com>
2023-04-07 12:29:45 +0100

    solution for task 3 - 3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,6 +15,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+
 	hash_node_t *val __attribute__((unused));
 	hash_node_t *temp __attribute__((unused));
 
